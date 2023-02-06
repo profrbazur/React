56697f163b9ab4cb20cbc72bc724a67c898aec96
profrbazurph <profrbazurph@gmail.com>
2023-02-07 05:42:52 +0800

    08 On Change Event

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
-import { Header } from "./components/Header";
-import { Footer } from "./components/Footer";
-import { TaskList } from "./components/TaskList";
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
+import { TaskList } from './components/TaskList';
+import { AddTask } from './components/AddTask';
 import './App.css';
 
 function App() {
-
   return (
     <div className="App">
       <Header />
+      <AddTask />
       <TaskList />
       <Footer />
     </div>
